bd4710cf106ab1f2cac656d2797c50680ddfd00a
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2013-01-25 12:19:31 +0100

    Use absolute paths

--- lib/libstreams.pc.cmake
+++ lib/libstreams.pc.cmake
@@ -1,10 +1,11 @@
 prefix=${CMAKE_INSTALL_PREFIX}
 exec_prefix=${CMAKE_INSTALL_PREFIX}/bin
 libdir=${LIB_DESTINATION}
-includedir=${CMAKE_INSTALL_PREFIX}/include
+includedir=${INCLUDE_DESTINATION}
 
 Name: libstreams
 Description: C++ streams for reading data as streams from various file formats
 Version: ${LIBSTREAMS_VERSION}
+
 Libs: -L${LIB_DESTINATION} -lstreams
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
+Cflags: -I${INCLUDE_DESTINATION}
